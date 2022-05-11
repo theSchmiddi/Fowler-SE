5c13995098c0b32f74130349b102c0e4da901929
theSchmiddi <marco.schmidt03@gmail.com>
2022-05-11 18:56:58 +0200

    Program main simplified

--- src/main/Program.java
+++ src/main/Program.java
@@ -16,8 +16,7 @@ public class Program
     
     public static void main(String args[]) 
     {
-        String result;
-        System.out.println("Welcome to the main.Movie Store");
+        System.out.println("Welcome to the Movie Store");
         Movie m1 = new Movie("movie1", 1);
         Movie m2 = new Movie("movie2", 2);
         Rental r1 = new Rental(m1, 10);
@@ -25,8 +24,7 @@ public class Program
         Customer c1 = new Customer("joe");
         c1.addRental(r1);   c1.addRental(r2);   
         System.out.println("Let's get the Statement");
-        result = c1.statement();
-        System.out.println(result);
+        System.out.println(c1.statement());
     }
 }
 
